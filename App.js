67146152592cc1e0ef8b9031b831ce9506e174a7
alestor123 <alestoraldous@gmail.com>
2021-04-03 15:47:28 +0530

    Emergency Push

--- App.js
+++ App.js
@@ -5,6 +5,6 @@ execSync(`git add . && git commit -m ' ${msg||'Emergency Push'} ' && git push -u
 }
 catch(err){
 console.log('We Got A Problem')
-console.log(err.stderr.toString()) 
+console.log(err.stdout.toString()) 
 }
 }
