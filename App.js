8bbeb1d86df392a840413db571f7b252d0ad4a47
alestor123 <alestoraldous@gmail.com>
2021-04-03 15:45:45 +0530

    Emergency Push

---
+++ App.js
@@ -0,0 +1,9 @@
+var {execSync} = require('child_process')
+module.exports = (msg,origin,branch) => {
+try {
+execSync(`git add . && git commit -m ' ${msg||'Emergency Push'} ' && git push -u ${origin || 'origin'} ${branch || 'master'}`)
+}
+catch(err){
+console.log('We Got A Problem')    
+}
+}
